a84b42275a103541e36ada75a476162cb3b0be17
Roman Danyliuk <romandanuliyk@gmail.com>
2025-11-04 22:35:49 +0200

    Added router and pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
+import AuthorizationLoginPage from '@/pages/AuthorizationLoginPage.vue'
+import AuthorizationRegistrationPage from '@/pages/AuthorizationRegistrationPage.vue'
+import CatalogPage from '@/pages/CatalogPage.vue'
+import HomePage from '@/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+    },
+    {
+      path: '/catalog',
+      component: CatalogPage,
+    },
+    {
+      path: '/login',
+      component: AuthorizationLoginPage,
+    },
+    {
+      path: '/register',
+      component: AuthorizationRegistrationPage,
+    }
+  ],
 })
 
 export default router
+
